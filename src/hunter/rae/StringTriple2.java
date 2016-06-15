13096df2e4b489498712666dee39f7a888b35f21
Stanislav.Kablukov <stasdolg@mail.ru>
2016-06-15 21:22:21 +0600

    Выделили отдельный класс для работы с конкретным типом, так будет нагляднее

---
+++ src/hunter/rae/StringTriple2.java
@@ -0,0 +1,11 @@
+package hunter.rae;
+
+/**
+ * Created by hunter on 15.06.16.
+ */
+public class StringTriple2 extends SimpleTriple<String> {
+
+    public StringTriple2(String left, String middle, String right) {
+        super(left, middle, right);
+    }
+}
